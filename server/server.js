8a4d19361ecde6cdac962ae027b7a3319bbaed5a
Beres McFarlane <beresmcfarlane@beress-air.home>
2018-02-24 17:43:17 +0000

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,16 +58,16 @@ app.get('/todos/:id', (req, res) => {
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id;
 
-  if(!ObjectID(id).isValid()) {
+  if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((deletedTodo) => {
-    if(!deletedTodo) {
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
       return res.status(404).send();
     }
 
-    res.send({deletedTodo})
+    res.send({todo});
   })
 }, (e) => {
   res.status(400).send();
